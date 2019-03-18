dd7f071276200136a86bf8e82691c232606b2b4a
lioujj <34760803+lioujj@users.noreply.github.com>
2019-03-18 18:29:06 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -6,11 +6,11 @@
 //% weight=0 color=#ff9933 icon="\uf233" block="LDM64*32 17.E"
 namespace LumexLDM6432_17E {
 
-    export enum showNow {
-        //% block="now"
-        yes = 0xd1,
-        //% block="later"
-        no = 0x00
+    export enum yesOrNo {
+        //% block="yes"
+        yes = 1,
+        //% block="no"
+        no = 0
     }
 
     export enum usrPatternType {
@@ -35,7 +35,7 @@ namespace LumexLDM6432_17E {
 
 
     //% blockId="LDM_changeColor" block="swap displayed color from color %color1| to color %color2"
-    //% weight=59 blockGap=2 color1.min=0 color1.max=111 color2.min=0 color2.max=111
+    //% weight=95 blockGap=2 color1.min=0 color1.max=111 color2.min=0 color2.max=111
     export function LDM_changeColor(color1: number, color2: number): void {
         serial.writeString("ATcc=(" + color1 + "," + color2 + ")")
         serial.readUntil("E")
@@ -43,15 +43,26 @@ namespace LumexLDM6432_17E {
     }
 
     //% blockId="LDM_setXYcolor" block="set the color code %color| to X: %x| Y: %y"
-    //% weight=59 blockGap=2 color.min=0 color.max=111 x.min=0 x.max=63 y.min=0 y.max=31
+    //% weight=90 blockGap=2 color.min=0 color.max=111 x.min=0 x.max=63 y.min=0 y.max=31
     export function LDM_setXYcolor(color: number, x: number, y: number): void {
         serial.writeString("ATee=(" + x + "," + y + "," + color + ")")
         serial.readUntil("E")
         basic.pause(3)
     }
 
+    //% blockId="LDM_colorEffect" block="set the color effect: %colorEffect"
+    //% weight=85 blockGap=2 colorEffect.min=1 colorEffect.max=9
+    export function LDM_colorEffect(colorEffect: number): void {
+        if (colorEffect>0 && colorEffect<10)
+        {
+            serial.writeString("ATc"+colorEffect+ "=()")
+            serial.readUntil("E")
+            basic.pause(3)
+        }
+    }
+
     //% blockId="LDM_saveDisplayed" block="save the whole display contents to RAM"
-    //% weight=34 blockGap=2
+    //% weight=80 blockGap=2
     export function LDM_saveDisplayed(): void {
         serial.writeString("AT2c=()")
         serial.readUntil("E")
@@ -59,16 +70,27 @@ namespace LumexLDM6432_17E {
     }
 
     //% blockId="LDM_loadDisplayed" block="load and show the whole display contents from RAM"
-    //% weight=33 blockGap=2
+    //% weight=75 blockGap=2
     export function LDM_loadDisplayed(): void {
         serial.writeString("AT2d=()")
         serial.readUntil("E")
         basic.pause(3)
     }
 
-    //% blockId="LDM_loadPattern" block="load user pattern from EEPROM|pattern type:%myPattern|Pattern ID: %myID to |X:%x|Y:%y|display %show"
-    //% weight=32 blockGap=2 x.min=0 x.max=63 y.min=0 y.max=31 myID.min=0 myID.max=24
-    export function LDM_loadPattern(myPattern: usrPatternType, myID: number, x: number, y: number, show: showNow): void {
+    //% blockId="LDM_setPatternOverlay" block="set pattern overlay on background: %myAns"
+    //% weight=70 blockGap=2
+    export function LDM_setPatternOverlay(myAns:yesOrNo): void {
+        if (myAns==1)
+            serial.writeString("AT2b=(0)")
+        else
+            serial.writeString("AT2b=(1)")
+        serial.readUntil("E")
+        basic.pause(1)
+    }
+
+    //% blockId="LDM_loadPattern" block="load user pattern from EEPROM|pattern type:%myPattern|Pattern ID: %myID to |X:%x|Y:%y|display now %show"
+    //% weight=65 blockGap=2 x.min=0 x.max=63 y.min=0 y.max=31 myID.min=0 myID.max=24
+    export function LDM_loadPattern(myPattern: usrPatternType, myID: number, x: number, y: number, show: yesOrNo): void {
         let myStr = "AT29=("
         if (show == 0)
             myStr = "AT2e=("
@@ -78,7 +100,7 @@ namespace LumexLDM6432_17E {
     }
 
     //% blockId="LDM_showAll" block="Display the multi patterns in the same time"
-    //% weight=31 blockGap=2
+    //% weight=60 blockGap=2
     export function LDM_showAll(): void {
         serial.writeString("AT2f=()")
         serial.readUntil("E")
